7327b8554e4c46022c998fc94463dd9fb239b83a
MogesBekele <mogesbekele32@gmail.com>
2025-09-11 08:13:27 +0300

    test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -22,30 +22,27 @@ describe("max", () => {
 
 describe("fizzBuzz", () => {
   it("should return Fizz if the number is divisible by 3", () => {
-    expect(fizzBuzz(9)).toBe('Fizz');
+    expect(fizzBuzz(9)).toBe("Fizz");
   });
-  it('should return Buzz if the number is divisible by 5',()=>{
-    expect(fizzBuzz(10)).toBe('Buzz')
-  })
-  it('should return FizzBuzz if the number is divisible by 3 and 5', ()=>{
-    expect(fizzBuzz(15)).toBe('FizzBuzz')
-  })
-  it('should return the number if it is not divisble by the numbers 3 and 5', ()=>{
-    expect(fizzBuzz(7)).toBe('7')
-  })
-  
+  it("should return Buzz if the number is divisible by 5", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+  it("should return FizzBuzz if the number is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("should return the number if it is not divisble by the numbers 3 and 5", () => {
+    expect(fizzBuzz(7)).toBe("7");
+  });
 });
 
-
-
-describe('factorial', ()=>{
-  it('should return 1 if the number it 0', ()=>{
-    expect(factorial(0)).toBe(1)
-  })
-  it('should return the number if it is 1', ()=>{
-    expect(factorial(1)).toBe(1)
-  })
-  it('should return the number if it is 2',()=>{
-    expect(factorial(3)).toBe(6)
-  })
-})
+describe("factorial", () => {
+  it("should return 1 if the number it 0", () => {
+    expect(factorial(0)).toBe(1);
+  });
+  it("should return the number if it is 1", () => {
+    expect(factorial(1)).toBe(1);
+  });
+  it("should return the number if it is 2", () => {
+    expect(factorial(3)).toBe(6);
+  });
+});
